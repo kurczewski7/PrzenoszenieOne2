265b25349e1393606cb5e62fca3ac8cd0dc70c30
Slawomir Kurczewski <kurczewski7@gmail.com>
2022-01-10 01:07:42 +0100

    Initial Commit

---
+++ PrzenoszenieOne/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PrzenoszenieOne
+//
+//  Created by Sławomir Kurczewski on 09/01/2022.
+//
+
+import UIKit
+import SSZipArchive
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        zaczynaj()
+    }
+    func zaczynaj() {
+        _ = SSZipArchive.unzipFile(atPath: "dane.zip", toDestination: "/DUPA")
+        //SSZipArchive.createZipFile(atPath:  , withContentsOfDirectory: <#T##String#>)
+        print("AAAAA")
+    }
+
+
+}
+
